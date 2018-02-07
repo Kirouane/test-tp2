556705baee72f00d6a77e1206691aa935bbf9d3f
samyl <irosam91@gmail.com>
2018-02-07 13:30:49 +0100

    added GET function

--- app.js
+++ app.js
@@ -12,19 +12,16 @@ app
 
     // Récupération d'un utilisateur
     .get(function (req, res) {
-        var id = req.params.id;
-
+       
         /**
          * Implémenter ce controlleur afin qu'il rechereche en base de donnée l'utilisateur par son ID et le retourne
          * sous format json au client.(voir le controlleur .post)
          */
 
-		 
-		 var repository = new UserRepository(db);
-         var user = repository.findOneById(id);
-         res.header("Access-Control-Allow-Origin", "*");
-		 res.send(user)
-        
+		 var id = req.params.id;
+         var repository = new UserRepository(db);
+         user = repository.findOneById(id);
+         res.send(user)
     })
 
     // Creation d'un utilisateur
